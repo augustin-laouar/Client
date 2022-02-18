97018138515bf587b2029f2420241f44b80f54c5
ThinhinaneZeddam <81924099+ThinhinaneZeddam@users.noreply.github.com>
2022-02-18 16:20:55 +0100

    ajout de documentation

--- ClientDessin/Cercle.h
+++ ClientDessin/Cercle.h
@@ -1,40 +1,88 @@
 #pragma once
 #include "FormeSimple.h"
 using namespace std;
+
+/** 
+* classe representant un cercle 
+*/
+
 class  Cercle : public FormeSimple
 {
 private:
 	double rayon;
 public:
+	/**
+	*@brief construction d'un cercle à partir de d'un centre qui est un point 2D avec coordonnees x,y
+	* @param centre rayon du centre
+	* @param rayon rayon du cercle
+	* @param couleur couleur du cercle
+	* 
+	*/
 	Cercle(const Point2D& centre, double rayon, int couleur) : FormeSimple(couleur) {
 		this->ajouterPoint(centre);
 		this->rayon = rayon;
 	}
+
+	/**
+	* @brief Constructeur par copie d'un cercle par copie
+	* @param c passage par reference
+	*/
 	Cercle(const Cercle& c) : FormeSimple(c.couleur) {
 		this->ajouterPoint(c.ListePoint[0]);
 		this->rayon = c.rayon;
 	}
+
+	/**
+	* @brief setter du centre
+	* @param c passage par reference du Point2D avec x,y comme coordonnees 
+	*/
 	void setCentre(const Point2D &c) {
 		this->ListePoint[0] = c;
 	}
+	/**
+	* @brief recuperer la valeur du centre
+	* @return Point2D 
+	*/
 	Point2D  getCentre()const {
 		return this->ListePoint[0];
 	}
+	/**
+	* @brief setter du rayon
+	* @param rayon 
+	*/
+
 	void setRayon(double rayon) {
 		if (rayon > 0) {
 			this->rayon = rayon;
 		}
 	}
+	/**
+	* @brief recuperer la valeur du rayon 
+	* @return double rayon 
+	*/
 	double getRayon()const {
 		return this->rayon;
 	}
+
+	/**
+	* @brief methode pour renvoyer un pointeur sur une copie d'un cercle
+	* @return Cercle*
+	*/
 	Cercle* clone()const {
 		return new Cercle(*this);
 	}
+
+	/**
+	* @brief transformer une chaine de caractere sous forme "cercle :centre :liste des points;rayon"
+	*/
 	string toString()const {
 		return "Cercle : centre : " + ListePoint[0].toString() + "; Rayon : " + to_string(rayon);
 	}
 
+	/**
+	* @brief operator = qui permet d'affecter un cercle a un autre 
+	* @return const Cercle& 
+	*/
 
 	virtual const Cercle& operator = (const Cercle& c) {
 		if (this == &c) {
@@ -45,6 +93,12 @@ public:
 		this->setCentre(c.getCentre());
 		return *this;
 	}
+
+	/**
+	* @brief operator d'egalite entre deux cercles 
+	* @return bool 
+	*/
+
 	virtual bool operator == (const Cercle &c)const {
 		if (this->rayon == c.rayon && c.ListePoint[0] == this->ListePoint[0]) {
 			
@@ -54,8 +108,16 @@ public:
 			return false;
 		}
 	}
+
+	/**
+	* @brief renvoi 10 pour indiquer que la forme est un cercle
+	* @return int 
+	*/
 	virtual int whoAmI()const {
 		return 10;
 	}
+	/**
+	* @brief methode necessaire pour le design pattern visitor
+	*/
 	virtual void accept(const VisitorForme2D* v);
 };
--- ClientDessin/Trait.h
+++ ClientDessin/Trait.h
@@ -3,10 +3,23 @@
 class Trait : public FormeSimple
 {
 public:
+
+	/**
+	* @brief constructeur 
+	* @param p1 premier point 
+	* @param p2 deuxieme point
+	* @param couleur couleur du trait 
+	* 
+	*/
 	Trait(const Point2D& p1, const Point2D& p2, int couleur): FormeSimple(couleur) {
 		this->ajouterPoint(p1);
 		this->ajouterPoint(p2);
 	}
+	/**
+	* @brief constructeur par copie avec passage par reference 
+	* @param t trait 
+	* @param couleur couleur du trait
+	*/
 	Trait(const Trait& t ) : FormeSimple(t.couleur) {
 		this->ajouterPoint(t.ListePoint[0]);
 		this->ajouterPoint(t.ListePoint[1]);
@@ -23,16 +36,27 @@ public:
 	Point2D  getP2()const {
 		return ListePoint[1];
 	}
+	/**
+	* @brief methode qui permet de retourner un pointeur sur une copie du trait
+	*/
 	Trait* clone()const {
 		return new Trait(*this);
 	}
 	
+	/**
+	* @brief transformer un trait en chaine de caractere
+	*/
 	
 	string toString()const {
 		string retour;
 		retour = "Trait : P1 = " + ListePoint[0].toString() + "; P2 = " + ListePoint[1].toString();
 		return retour;
 	}
+	/**
+	* @brief affectation d'un trait a un autre 
+	* @param t une reference sur un trait
+	* @return const trait& on retourne une reference sur le trait
+	*/
 
 
 	virtual const Trait& operator = (const Trait& t) {
@@ -44,6 +68,11 @@ public:
 		this->setP2(t.ListePoint[1]);
 		return *this;
 	}
+
+	/**
+	* @brief verifie l'egalite entre deux trait 
+	* @param t const trait& 
+	*/
 	virtual bool operator == (const Trait &t)const {
 		
 		if (  (ListePoint[0] == t.ListePoint[0]) && (ListePoint[1] == t.ListePoint[1]))
@@ -54,8 +83,16 @@ public:
 			return false;
 		}
 	}
+
+	/**
+	* @brief renvoi 12 pour dire que c'est un trait
+	*/
 	virtual int whoAmI()const {
 		return 12;
 	}
+	/**
+	* @brief methode pour visitor 
+	* @param v VisitorForme2d*
+	*/
 	virtual void accept(const VisitorForme2D* v);
 };
