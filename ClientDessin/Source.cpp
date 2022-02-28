22c45a9e90a37cf030734bf27099ca5e78535187
LAOUARAugustin <55484260+augustin-laouar@users.noreply.github.com>
2022-02-28 18:57:42 +0100

    Correction MondeEcran

--- ClientDessin/Source.cpp
+++ ClientDessin/Source.cpp
@@ -294,60 +294,23 @@ int main() {
 //	}
 //	*/
 
-    Vecteur2D v1(150, 200);
-	Vecteur2D v2(25, 10);
-
-	Vecteur2D p56;
-	Vecteur2D p57;
-	Vecteur2D p58;
-	
-	
-
-	Vecteur2D p1(45, 58);
-	
-	
-	p56.x = 410;
-	p56.y = 450;
-	
-	p57.x = 480;
-	p57.y = 525;
-	p58.x = 430;
-	p58.y = 442;
-
-	Couleur c = c.Red();
-	Cercle c1(p1, 120, c);
-	Cercle c2(p1, 110,Couleur::Green());
-
-	Trait* t85 = new Trait(p56, p57,c);
-	vector<Vecteur2D>Points;
-	Vecteur2D p100(312, 400);
-	Vecteur2D p101(512, 145);
-	Vecteur2D p102(412, 320);
-	Points.push_back(p100);
-	Points.push_back(p101);
-	Points.push_back(p102);
-	Polygone* po85 = new Polygone(Points,c);
-	Plan2D plan;
-	plan.ajouterForme(t85);
-	plan.ajouterForme(&c1);
-	plan.ajouterForme(&c2);
-	plan.ajouterForme(po85);
-	Trait* t86 = new Trait(*t85);
-	t86->translation(v1);
-	t86->setCouleur(Couleur::Cyan());
-	Polygone* po86 = new Polygone(*po85);
-	Vecteur2D p103(390, 400);
-	po86->setCouleur(Couleur::Red());
-	plan.ajouterForme(po86);
-	plan.ajouterForme(t86);
-	Vecteur2D p12(-1.5, -1.5);
-	Vecteur2D p22(400, 400);
-	Trait t1(p12, p22, Couleur::Black());
-	plan.ajouterForme(&t1);
-	Dessin d2(plan, 800, 500);
-	d2.Dessiner();
-	//Dessin d(plan,300,300);
-	//d.Dessiner();
+Vecteur2D p1(1, 2);
+Vecteur2D p2(-2.1, 2);
+Vecteur2D p3(1.2, 2.2);
+Vecteur2D p4(0, 3.1);
+vector<Vecteur2D> v;
+v.push_back(p1);
+v.push_back(p2);
+v.push_back(p3);
+Cercle c(p1, 1.5, Couleur::Blue());
+Trait t(p2, p3, Couleur::Black());
+Triangle tr(p2, p3, p4, Couleur::Red());
+Plan2D plan;
+plan.ajouterForme(&c);
+plan.ajouterForme(&t);
+plan.ajouterForme(&tr);
+Dessin d(plan,800, 300);
+d.Dessiner();
 	Communication* com = com->getInstance();
 	com->FermerConnexion();
 
